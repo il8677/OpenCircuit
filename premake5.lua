36b638e21aef8fd5dd9d2dbb69c2b06df5414ca3
il8677 <jiajiaisaac@gmail.com>
2021-04-13 05:58:15 +0200

    Added premake & test code

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+workspace "OpenCircuit"
+    configurations {"Debug", "Release"}
+
+
+project "OpenCircuit"
+    kind "WindowedApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files {"**.h", "**.cpp"}
+
+    includedirs{
+        "%{prj.name}/depen/SFML/include"
+    }
+
+    defines "SFML_STATIC"
+
+    links{
+            "opengl32.lib",
+            "freetype.lib",
+            "winmm.lib",
+            "gdi32.lib"
+    }
+
+    filter "configurations:Debug"
+        links{
+            "sfml-window-s-d.lib",
+            "sfml-system-s-d.lib"
+        }
+        symbols "On"
+        
+    filter "configurations:Release"
+        links{
+            "sfml-window-s.lib",
+            "sfml-system-s.lib"
+        }
+        optimize "On"
