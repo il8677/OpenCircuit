434b13353901940bc7d2f7f7dac41a06dbf0eab3
il8677 <jiajiaisaac@gmail.com>
2021-04-27 05:09:52 +0200

    Added test premake file for linux

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,7 @@ project "OpenCircuit"
     targetdir "bin/%{cfg.buildcfg}"
     architecture "x64"
 
+	cppdialect "C++17"
     files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
 
     includedirs{
@@ -19,40 +20,49 @@ project "OpenCircuit"
         "%{prj.name}/depen/SFML/lib",
     }
 
-    defines "SFML_STATIC"
+	-- Experimental linux
 
-    links{
-            "opengl32.lib",
-            "freetype.lib",
-            "winmm.lib",
-            "gdi32.lib",
-            "openal32.lib",
-            "flac.lib",
-            "vorbisenc.lib",
-            "vorbisfile.lib",
-            "vorbis.lib",
-            "ogg.lib"
-    }
+	if _ACTION=="gmake" then
+		links {
+				"GL",
+				"sfml-graphics",
+				"sfml-window",
+				"sfml-system"
+			}
+	end
+	if _ACTION=="vs2019" then
+		links{
+				"opengl32.lib",
+				"freetype.lib",
+				"winmm.lib",
+				"gdi32.lib",
+				"openal32.lib",
+				"flac.lib",
+				"vorbisenc.lib",
+				"vorbisfile.lib",
+				"vorbis.lib",
+				"ogg.lib"
+		}
 
-    filter "configurations:Debug"
-        links{
-            "sfml-graphics-s-d.lib",
-            "sfml-window-s-d.lib",
-            "sfml-system-s-d.lib",
-            "sfml-network-s-d.lib"
-        }
+		filter "configurations:Debug"
+			links{
+				"sfml-graphics-s-d.lib",
+				"sfml-window-s-d.lib",
+				"sfml-system-s-d.lib",
+				"sfml-network-s-d.lib"
+			}
 
-    	defines "SFML_STATIC"
-        symbols "On"
-        
-    filter "configurations:Release"
-        links{
-            "sfml-graphics-s.lib",
-            "sfml-window-s.lib",
-            "sfml-system-s.lib",
-            "sfml-network-s.lib"
-        }
-        optimize "On"
+			defines "SFML_STATIC"
+			symbols "On"
+			
+		filter "configurations:Release"
+			links{
+				"sfml-graphics-s.lib",
+				"sfml-window-s.lib",
+				"sfml-system-s.lib",
+				"sfml-network-s.lib"
+			}
+			optimize "On"
 
-    filter {}
-
+		filter {}
+	end
