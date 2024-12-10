55037d0f06eb5a85698fa60f7421307ffa426b8e
il8677 <jiajiaisaac@gmail.com>
2024-12-10 19:18:45 +0100

    Removed collapsing headers

--- OpenCircuit/src/UI/SimulationManager.cpp
+++ OpenCircuit/src/UI/SimulationManager.cpp
@@ -7,38 +7,36 @@
 
 void SimulationManager::onImGuiDraw(){
     ImGui::Begin("Simulation Manager");
-    if (ImGui::CollapsingHeader("Inputs")) {
-        std::vector<char*> inputs = m_chunk.getInputs();
-        for (int i = 0; i < inputs.size(); i++) {
-            ImGui::PushID(i);
-            if (i > 0)
-                ImGui::SameLine();
-            if (ImGui::Checkbox(" ", (bool*)inputs[i])) {
-                m_chunk.updateInputs();
-            }
-            ImGui::PopID();
+    ImGui::SeparatorText("Inputs");
+    std::vector<char*> inputs = m_chunk.getInputs();
+    for (int i = 0; i < inputs.size(); i++) {
+        ImGui::PushID(i);
+        if (i > 0)
+            ImGui::SameLine();
+        if (ImGui::Checkbox(" ", (bool*)inputs[i])) {
+            m_chunk.updateInputs();
         }
+        ImGui::PopID();
     }
-    if (ImGui::CollapsingHeader("Outputs")) {
 
-        std::vector<char*> outputs = m_chunk.getOutputs();
-        for (int i = 0; i < outputs.size(); i++) {
-            if (i > 0)
-                ImGui::SameLine();
-            ImGui::Text("%d", *outputs[i]);
-        }
+    ImGui::SeparatorText("Outputs");
+    std::vector<char*> outputs = m_chunk.getOutputs();
+    for (int i = 0; i < outputs.size(); i++) {
+        if (i > 0)
+            ImGui::SameLine();
+        ImGui::Text("%d", *outputs[i]);
     }
-    if (ImGui::CollapsingHeader("Controls")) {
-        if (ImGui::Button("Reset")) {
-            m_chunk.reset();
-        }
-        if (ImGui::Button("Tick")) {
-            m_chunk.tick();
-        }
 
-        ImGui::Text("Autotick");
-        ImGui::SliderInt("Tick period", &m_autoTickAmount, 1, 100);
-        ImGui::Checkbox("Do autotick", &m_doAutotick);
+    ImGui::SeparatorText("Controls");
+    if (ImGui::Button("Reset")) {
+        m_chunk.reset();
     }
+    if (ImGui::Button("Tick")) {
+        m_chunk.tick();
+    }
+
+    ImGui::Text("Autotick");
+    ImGui::SliderInt("Tick period", &m_autoTickAmount, 1, 100);
+    ImGui::Checkbox("Do autotick", &m_doAutotick);
     ImGui::End();
 }
--- OpenCircuit/src/UI/WorkspacePanel.cpp
+++ OpenCircuit/src/UI/WorkspacePanel.cpp
@@ -9,7 +9,7 @@
 void WorkspacePanel::onImGuiDraw(){
     const ImGuiWindowFlags menuBarFlags=0;
     ImGui::Begin("Workspace", NULL, menuBarFlags);
-    ImGui::Text("Schematics");
+    ImGui::SeparatorText("Schematics");
 
     if (ImGui::Button("Save")) {
         std::ofstream fs;
@@ -25,44 +25,42 @@ void WorkspacePanel::onImGuiDraw(){
         fs.close();
     }
 
-    if (ImGui::CollapsingHeader("New schematic")) {
-        static char newSchematicName[20];
-        ImGui::InputText("Name", newSchematicName, 20);
-        if (ImGui::Button("Create")) {
-            m_workspace.newSchematic(std::string(newSchematicName));
-        }
+    ImGui::SeparatorText("New schematic");
+    static char newSchematicName[20];
+    ImGui::InputText("Name", newSchematicName, 20);
+    if (ImGui::Button("Create")) {
+        m_workspace.newSchematic(std::string(newSchematicName));
     }
 
-    if (ImGui::CollapsingHeader("Edit schematics")) {
-        for (int i = 0; i < m_workspace.schematicCount(); i++) {
-            ImGui::PushID(i);
-            if (ImGui::Button("v")) {
-                m_workspace.moveUp(i);
-            }
-            ImGui::SameLine();
-            ImGui::Text("%s", m_workspace.getSchematic(i)->getName().c_str());
-            ImGui::SameLine();
-
-            if (ImGui::Button("Edit")) {
-                auto e = SchematicChangedEvent(m_workspace.getSchematic(i));
-                dispatchEvent(&e);
-            }
-            if (i > 0) {
-                ImGui::SameLine();
-                if (ImGui::Button("x")) {
-                    m_workspace.deleteSchematic(i);
-                }
-
-            }
-            //Subcircuits can only be placed in circuits *below* the current circuit
-            // At some point the UI should enforce that
-            ImGui::SameLine();
-            if (ImGui::Button("Place")) {
-                auto e = SchematicPlaceEvent(m_workspace.getSchematic(i));
-                dispatchEvent(&e);
-            }
-            ImGui::PopID();
+    ImGui::SeparatorText("Edit schematics");
+    for (int i = 0; i < m_workspace.schematicCount(); i++) {
+        ImGui::PushID(i);
+        if (ImGui::Button("v")) {
+            m_workspace.moveUp(i);
         }
+        ImGui::SameLine();
+        ImGui::Text("%s", m_workspace.getSchematic(i)->getName().c_str());
+        ImGui::SameLine();
+
+        if (ImGui::Button("Edit")) {
+            auto e = SchematicChangedEvent(m_workspace.getSchematic(i));
+            dispatchEvent(&e);
+        }
+        if (i > 0) {
+            ImGui::SameLine();
+            if (ImGui::Button("x")) {
+                m_workspace.deleteSchematic(i);
+            }
+
+        }
+        //Subcircuits can only be placed in circuits *below* the current circuit
+        // At some point the UI should enforce that
+        ImGui::SameLine();
+        if (ImGui::Button("Place")) {
+            auto e = SchematicPlaceEvent(m_workspace.getSchematic(i));
+            dispatchEvent(&e);
+        }
+        ImGui::PopID();
     }
     ImGui::End();
 }
